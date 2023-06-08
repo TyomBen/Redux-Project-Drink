f3862e6a9a8325e10e2e39dd6c9dda1bd6c4aa89
TyomBen <artyombenikyan@mail.ru>
2023-06-08 12:53:13 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          {paths.map(({ path, element }) => {
-            return <Route path={path} element={element} />;
+          {paths.map(({ path, element }) => {return <Route path={path} element={element} />;
           })}
         </Routes>
       </BrowserRouter>
